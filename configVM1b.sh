8ea61a0f1e920e2d1bed1fbde743380a6e08d6b9
Maycon Douglas (Personal) <maycon.santos2@fatec.sp.gov.br>
2019-06-28 00:49:30 -0300

    Update configVM1b.sh

--- configVM1b.sh
+++ configVM1b.sh
@@ -6,15 +6,15 @@ echo "${green}Instalando Uploader Server${reset}"
 sudo git clone https://github.com/mayconht/Projeto_Final
 cd Projeto_Final
 cd UploaderServer
-sudo docker build -t uploaderServer:latest .
-sudo docker run -v /data/:/var/files/ -d -p 5000:5000 uploaderServer:latest --name="uploaderServer"
+sudo docker build -t uploaderserver:latest .
+sudo docker run -v /data/:/var/files/ -d -p 5000:5000 uploaderserver:latest --name="uploaderserver"
 
 echo "${green}Instalando Uploader Client${reset}"
 sudo git clone https://github.com/mayconht/Projeto_Final
 cd Projeto_Final
 cd UploaderClient
-sudo docker build -t uploaderClient:latest .
-sudo docker run -v /data/:/var/files/ -d -p 5001:5001 uploaderClient:latest --name="uploaderClient"
+sudo docker build -t uploaderclient:latest .
+sudo docker run -v /data/:/var/files/ -d -p 5001:5001 uploaderclient:latest --name="uploaderclient"
 
 
 
