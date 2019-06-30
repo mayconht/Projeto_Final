e8a7692ff1bd85e76925bff240dbfb6b0b8aea16
Maycon Douglas (Personal) <maycon.santos2@fatec.sp.gov.br>
2019-06-30 05:12:10 -0300

    SH

--- configVM2a.sh
+++ configVM2a.sh
@@ -5,7 +5,6 @@ echo "${red}Este Programa foi criado pelo: ${green}Grupo 4 ${reset}"
 
 echo "${green}Installing Docker${reset}"
 sudo apt update
-sudo apt-get --yes --force-yes upgrade
 sudo apt -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
--- configVM2b.sh
+++ configVM2b.sh
@@ -5,5 +5,5 @@ reset=`tput sgr0`
 echo "${green}Instalando Prometheus${reset}"
 wget https://s3-eu-west-1.amazonaws.com/deb.robustperception.io/41EFC99D.gpg | sudo apt-key add -
 apt-get update
-apt-get install prometheus prometheus-node-exporter prometheus-pushgateway prometheus-alertmanager
+apt -y install prometheus prometheus-node-exporter prometheus-pushgateway prometheus-alertmanager
 service prometheus status
