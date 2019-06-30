e5614d832aa2c7bcb838465b78731f4b4d0633c8
Maycon Douglas (Personal) <maycon.santos2@fatec.sp.gov.br>
2019-06-29 23:30:53 -0300

    Finalizado Upload Server / Client

--- configVM2b.sh
+++
@@ -1,4 +0,0 @@
-sudo wget https://github.com/prometheus/node_exporter/releases/download/v0.15.2/node_exporter-0.15.2.linux-amd64.tar.gz
-sudo tar -xf node_exporter-0.15.2.linux-amd64.tar.gz
-sudo sudo mv node_exporter-0.15.2.linux-amd64/node_exporter /usr/local/bin
-sudo rm -r node_exporter-0.15.2.linux-amd64*
