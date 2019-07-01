64bbc831f79cf4c8b8655ca299ecbed12267e273
Maycon Douglas (Personal) <maycon.santos2@fatec.sp.gov.br>
2019-07-01 04:08:40 -0300

    Update configVM2b.sh

--- configVM2b.sh
+++ configVM2b.sh
@@ -6,4 +6,9 @@ echo "${green}Instalando Prometheus${reset}"
 wget https://s3-eu-west-1.amazonaws.com/deb.robustperception.io/41EFC99D.gpg | sudo apt-key add -
 apt-get update
 apt -y install prometheus prometheus-node-exporter prometheus-pushgateway prometheus-alertmanager
-service prometheus status
+sudo systemctl stop prometheus
+sudo chmod -R 777 /etc/prometheus/
+cd /etc/prometheus/
+sudo rm -rf prometheus.yml
+wget https://github.com/mayconht/Projeto_Final/blob/master/Prometheus/prometheus.yml
+sudo systemctl start prometheus
