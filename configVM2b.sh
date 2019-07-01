3c4bdf29a1d3a4f0bbe96aa1c8f068d3484835af
Maycon Douglas (Personal) <maycon.santos2@fatec.sp.gov.br>
2019-07-01 04:49:28 -0300

    prometheus

--- configVM2b.sh
+++ configVM2b.sh
@@ -10,5 +10,5 @@ sudo systemctl stop prometheus
 sudo chmod -R 777 /etc/prometheus/
 cd /etc/prometheus/
 sudo rm -rf prometheus.yml
-wget https://github.com/mayconht/Projeto_Final/blob/master/Prometheus/prometheus.yml
+wget https://raw.githubusercontent.com/mayconht/Projeto_Final/master/Prometheus/prometheus.yml
 sudo systemctl start prometheus
