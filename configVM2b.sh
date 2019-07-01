1d20e5f34efdacc9a4c667f0b64e012acf483904
Maycon Douglas (Personal) <maycon.santos2@fatec.sp.gov.br>
2019-07-01 05:17:57 -0300

    Adding Grafana

--- configVM2b.sh
+++ configVM2b.sh
@@ -12,3 +12,12 @@ cd /etc/prometheus/
 sudo rm -rf prometheus.yml
 wget https://raw.githubusercontent.com/mayconht/Projeto_Final/master/Prometheus/prometheus.yml
 sudo systemctl start prometheus
+
+cd ~
+
+echo "${green}Instalando Grafana${reset}"
+url https://packagecloud.io/gpg.key | sudo apt-key add -
+sudo apt-get update
+sudo apt -y install grafana
+sudo systemctl start grafana-server
+sudo systemctl enable grafana-server
