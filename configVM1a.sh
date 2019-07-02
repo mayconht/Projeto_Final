4168b0f133ac2e9abfb86b49c1d655456a2aee07
Maycon Douglas (Personal) <maycon.santos2@fatec.sp.gov.br>
2019-07-02 13:40:31 -0300

    Update configVM1a.sh

--- configVM1a.sh
+++ configVM1a.sh
@@ -4,12 +4,12 @@ reset=`tput sgr0`
 echo "${red}Este Programa foi criado pelo: ${green}Grupo 4 ${reset}"
 
 echo "${green}Installing Docker${reset}"
-
+sudo apt update
+sudo apt -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt update
-sudo apt -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 sudo apt -y install docker-ce docker-ce-cli containerd.io
 sudo apt -y install docker-compose
 sudo usermod -aG docker $USER
