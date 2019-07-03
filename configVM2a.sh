aae1e434d4767f0821195e1846cf9fb8b7563c64
Maycon Douglas Silva Santos <maycon.santos2@fatec.sp.gov.br>
2019-07-03 10:33:33 -0300

    Update configVM2a.sh

--- configVM2a.sh
+++ configVM2a.sh
@@ -44,9 +44,10 @@ cd ~
 
 echo "${green}Instalando Prometheus${reset}"
 wget https://s3-eu-west-1.amazonaws.com/deb.robustperception.io/41EFC99D.gpg | sudo apt-key add -
-sudo apt-get update
-sudo apt -y install prometheus prometheus-node-exporter prometheus-pushgateway prometheus-alertmanager
-sudo systemctl stop prometheus
+# sudo apt-get update
+# sudo apt -y install prometheus prometheus-node-exporter prometheus-pushgateway prometheus-alertmanager
+# sudo systemctl stop prometheus
+sudo mkdir /etc/prometheus/
 sudo chmod -R 777 /etc/prometheus/
 cd /etc/prometheus/
 sudo rm -rf prometheus.yml
@@ -54,6 +55,3 @@ wget https://raw.githubusercontent.com/mayconht/Projeto_Final/master/Prometheus/
 wget https://raw.githubusercontent.com/mayconht/Projeto_Final/master/Prometheus/docker-compose.yml
 sudo docker-compose up &
 sudo docker-compose ps
-
-cd /etc/prometheus/
-sudo docker-compose up &
